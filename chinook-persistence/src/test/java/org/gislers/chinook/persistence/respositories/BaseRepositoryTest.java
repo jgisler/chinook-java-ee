fb4051186dd7f9f2d18a32066e37e820039af204
James Gisler <james.gisler@nike.com>
2015-09-09 11:19:44 -0700

    renamed config class

--- chinook-persistence/src/test/java/org/gislers/chinook/persistence/respositories/BaseRepositoryTest.java
+++ chinook-persistence/src/test/java/org/gislers/chinook/persistence/respositories/BaseRepositoryTest.java
@@ -1,6 +1,6 @@
 package org.gislers.chinook.persistence.respositories;
 
-import org.gislers.chinook.persistence.config.AppConfig;
+import org.gislers.chinook.persistence.config.PersistenceConfig;
 import org.gislers.chinook.persistence.test.support.JndiDatasourceHelper;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
@@ -10,10 +10,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 /**
- * Created by jim on 9/5/15.
+ * Created by:   jim
+ * Created date: 9/5/15
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=AppConfig.class)
+@ContextConfiguration(classes=PersistenceConfig.class)
 @TestExecutionListeners(listeners = {DependencyInjectionTestExecutionListener.class})
 public abstract class BaseRepositoryTest {
 
