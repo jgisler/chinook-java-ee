39ed15a9cfaeec992273d0862a95550d981a63d6
James Gisler <james.gisler@nike.com>
2015-09-09 16:10:30 -0700

    custom query

--- chinook-persistence/src/main/java/org/gislers/chinook/persistence/respositories/AlbumRepository.java
+++ chinook-persistence/src/main/java/org/gislers/chinook/persistence/respositories/AlbumRepository.java
@@ -2,10 +2,12 @@ package org.gislers.chinook.persistence.respositories;
 
 import org.gislers.chinook.persistence.respositories.entities.AlbumEntity;
 
+import java.util.List;
+
 /**
  * Created by jim on 8/31/15.
  */
 public interface AlbumRepository extends BaseRepository<AlbumEntity, Integer> {
 
-
+    List<AlbumEntity> findByArtistIdOrderByTitleAsc( Integer artistId );
 }
--- chinook-persistence/src/test/java/org/gislers/chinook/persistence/respositories/AlbumEntityRepositoryTest.java
+++ chinook-persistence/src/test/java/org/gislers/chinook/persistence/respositories/AlbumEntityRepositoryTest.java
@@ -23,27 +23,12 @@ public class AlbumEntityRepositoryTest extends BaseRepositoryTest {
     private AlbumRepository repository;
 
     @Test
-    public void testSave() {
-
-        AlbumEntity albumEntity = new AlbumEntity();
-        albumEntity.setArtistId(1);
-        albumEntity.setTitle("Test title");
-
-        albumEntity = repository.save(albumEntity);
-        assertEquals( new Integer(1), albumEntity.getArtistId() );
-        assertEquals( "Test title", albumEntity.getTitle() );
-
-        repository.delete(albumEntity);
-    }
-
-    @Test
     public void testFind() {
         AlbumEntity entity = repository.findOne(1);
         assertNotNull(entity);
 
         assertEquals(new Integer(1), entity.getAlbumId());
         assertEquals("For Those About To Rock We Salute You", entity.getTitle());
-        assertEquals(new Integer(1), entity.getArtistId());
     }
 
     @Test
@@ -59,5 +44,20 @@ public class AlbumEntityRepositoryTest extends BaseRepositoryTest {
             logger.debug( albumEntity.toString() );
         }
         assertNotNull(albumEntityList);
+    }
+
+    @Test
+    public void testFindByArtistIdOrderByTitleAsc() {
+        List<AlbumEntity> albumEntities = repository.findByArtistIdOrderByTitleAsc( 1 );
+        assertFalse( albumEntities.isEmpty() );
+        assertEquals( 2, albumEntities.size() );
+
+        AlbumEntity albumEntity = albumEntities.get(0);
+        assertEquals( new Integer(1), albumEntity.getAlbumId() );
+        assertEquals("For Those About To Rock We Salute You", albumEntity.getTitle());
+
+        albumEntity = albumEntities.get(1);
+        assertEquals( new Integer(4), albumEntity.getAlbumId() );
+        assertEquals( "Let There Be Rock", albumEntity.getTitle() );
     }
 }
